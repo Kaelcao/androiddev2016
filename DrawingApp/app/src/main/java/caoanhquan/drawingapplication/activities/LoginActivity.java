b0e4c6214eced25deeedd4a2489daf9523d28eee
Cao Anh Quan <aquancva@gmail.com>
2016-03-11 11:47:39 +0700

    Application View Design Version 0.1

---
+++ DrawingApp/app/src/main/java/caoanhquan/drawingapplication/activities/LoginActivity.java
@@ -0,0 +1,38 @@
+package caoanhquan.drawingapplication.activities;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import caoanhquan.drawingapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        getSupportActionBar().hide();
+        mLoginButton = (Button) findViewById(R.id.btn_login);
+        mRegisterButton = (Button) findViewById(R.id.btn_register);
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ DrawingApp/app/src/main/java/caoanhquan/drawingapplication/activities/MainActivity.java
@@ -0,0 +1,71 @@
+package caoanhquan.drawingapplication.activities;
+
+import android.app.ActionBar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import caoanhquan.drawingapplication.R;
+import caoanhquan.drawingapplication.fragments.DrawFragment;
+import caoanhquan.drawingapplication.fragments.OnlineDrawFragment;
+import caoanhquan.drawingapplication.fragments.SavedDrawFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
+    }
+}
+
+class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 3;
+
+    final String[] titles = {"Drawing Area", "Saved Draws", "Online Draws"};
+
+    public HomeFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        DrawFragment drawFragment = new DrawFragment();
+        OnlineDrawFragment onlineDrawFragment = new OnlineDrawFragment();
+        SavedDrawFragment savedDrawFragment = new SavedDrawFragment();
+
+        switch (position) {
+            case 0:
+                return drawFragment;
+            case 1:
+                return onlineDrawFragment;
+            case 2:
+                return savedDrawFragment;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
