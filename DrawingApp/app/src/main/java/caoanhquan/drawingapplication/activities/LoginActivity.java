010392a451a814b8a0b1286961b396b8da9d0168
Cao Anh Quan <aquancva@gmail.com>
2016-03-25 14:27:30 +0700

    them network

--- DrawingApp/app/src/main/java/caoanhquan/drawingapplication/activities/LoginActivity.java
+++ DrawingApp/app/src/main/java/caoanhquan/drawingapplication/activities/LoginActivity.java
@@ -4,8 +4,20 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import caoanhquan.drawingapplication.R;
 
@@ -13,6 +25,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button mLoginButton;
     private Button mRegisterButton;
+    private EditText mEmail;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +35,35 @@ public class LoginActivity extends AppCompatActivity {
 //        getSupportActionBar().hide();
         mLoginButton = (Button) findViewById(R.id.btn_login);
         mRegisterButton = (Button) findViewById(R.id.btn_register);
+        mEmail = (EditText) findViewById(R.id.email);
+        final RequestQueue queue = Volley.newRequestQueue(this);
         final Intent intent = new Intent(getApplicationContext(), DrawActivity.class);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intent);
+                StringRequest request = new StringRequest("http://dev.colorme.vn/api/getuserdata?email=" + mEmail.getText(), new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("USTHWeather", "Json response " + response);
+                        JSONObject obj = null;
+                        try {
+                            obj = new JSONObject(response);
+                            String name = obj.getString("name");
+                            Toast.makeText(getApplicationContext(), "Welcome " + name, Toast.LENGTH_LONG).show();
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                            }
+                        });
+
+                queue.add(request);
             }
         });
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
